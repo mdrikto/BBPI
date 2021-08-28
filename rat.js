274bbb746b7f64d8743e9eb7799a939159a5b99d
rikto <73441847+mdrikto@users.noreply.github.com>
2021-08-28 18:04:26 +0600

    Add files via upload

---
+++ rat.js
@@ -0,0 +1,11 @@
+x = document.querySelector("title");
+a = document.querySelector(".cmt1");
+b = document.querySelector(".cmt3");
+c = document.querySelector(".cmt5");
+d = document.querySelector(".cmt7");
+
+x.innerHTML="BBPI";
+a.innerHTML="1 Semestr";
+b.innerHTML="3 Semestr";
+c.innerHTML="5 Semestr";
+d.innerHTML="7 Semestr";
